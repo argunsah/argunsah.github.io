a287584e633d723710eded041d49106fdbea339e
argunsah <aoargunsah@gmail.com>
2025-01-15 09:31:40 +0000

    Deploying to gh-pages from @ argunsah/argunsah.github.io@10c9b631c53317bd150b43b44daeb1fda3f22dce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,16 +409,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-together-with-ertunç-erdil-and-devrim-ünay-we-just-finished-writing-a-book-chapter-called-applications-of-computer-vision-and-machine-learning-in-bioimaging-that-will-appear-in-the-book-called-bioimaging-modalities-in-bioengineering",
+          title: 'Together with Ertunç Erdil and Devrim Ünay, we just finished writing a book...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
