f042268617c9b91e61ec8f0d9aa42da776bf8fae
argunsah <aoargunsah@gmail.com>
2025-01-15 15:03:07 +0000

    Deploying to gh-pages from @ argunsah/argunsah.github.io@80b7b46df71e1af480ac87856c0e4ff20c807979 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "nav-people",
           title: "people",
