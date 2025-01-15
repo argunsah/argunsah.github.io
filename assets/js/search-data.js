c7f6e58081074a2a5f7dd76a17df2d3236a0f10f
argunsah <aoargunsah@gmail.com>
2025-01-15 15:09:16 +0000

    Deploying to gh-pages from @ argunsah/argunsah.github.io@866a2d84b4e986796c4d3fb223f9e062372ea7ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,6 +51,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
